90e3dca816dc143f2eaaecd5c39d95ec2f6b5f3a
darday <darday1980@gmail.com>
2022-10-11 22:31:42 -0500

    Modelo Equipo Creado

---
+++ database/migrations/2022_10_11_221452_create_equipment_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id('equipment_id');
+            $table->string('name');
+            $table->text('description');
+            $table->float('cost');
+            $table->string('img_1');
+            $table->boolean('state');
+            $table->string('type');
+            $table->float('discount');
+            $table->string('discount_description');
+            $table->string('contact_phone');
+            $table->string('messagge_for_contact');
+            $table->string('varchar_1')->nullable();
+            $table->string('varchar_2')->nullable();
+            $table->string('varchar_3')->nullable();
+            $table->integer('last_user')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+}
